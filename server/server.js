d91567440fd3bb0a851dc21815c79e0818d2b632
Sebastian Lupascu <91190993+Golack24@users.noreply.github.com>
2023-02-03 01:19:38 +0000

    Update server.js

--- server/server.js
+++ server/server.js
@@ -19,7 +19,6 @@ import { MongoClient, ServerApiVersion } from 'mongodb'
 
 
 
-dotenv.config({path: '/etc/secrets/.env'})
 
 const configuration = new Configuration({
   apiKey: "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",
