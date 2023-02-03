9ba6560d195ef4e7c124077c5796e6fdc89ed239
Sebastian Lupascu <91190993+Golack24@users.noreply.github.com>
2023-02-03 11:52:00 +0000

    Update server.js

--- server/server.js
+++ server/server.js
@@ -32,7 +32,7 @@ app.post('/', async (req, res) => {
 
       const response = await openai.createCompletion({
         model: "text-davinci-003",
-        prompt: `I want you to act as a mental health adviser. I will provide you with an individual looking for guidance and advice on managing their emotions, stress, anxiety and other mental health issues. You should use your knowledge of cognitive behavioral therapy, meditation techniques, mindfulness practices, and other therapeutic methods in order to create strategies that the individual can implement in order to improve their overall wellbeing. My first request is "I need someone who can help me manage my depression symptoms: ${prompt} `,
+        prompt: `I want you to act as a mental health adviser. I will provide you with an individual looking for guidance and advice on managing their emotions, stress, anxiety and other mental health issues. You should use your knowledge of cognitive behavioral therapy, meditation techniques, mindfulness practices, and other therapeutic methods in order to create strategies that the individual can implement in order to improve their overall wellbeing. My first request is "I need someone who can help me manage my depression symptoms `,
         temperature: 0.8,
         max_tokens: 200,
         top_p: 1,
