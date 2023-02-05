52fec7bedb386f1c53847e92d55b3a75731d9237
Sebastian Lupascu <91190993+Golack24@users.noreply.github.com>
2023-02-05 19:32:04 +0000

    f

--- client/script.js
+++ client/script.js
@@ -3,9 +3,9 @@ import user from './assets/user.svg'
 
 const form = document.querySelector('form')
 const chatContainer = document.querySelector('#chat_container')
-const voices = window.speechSynthesis.getVoices();
-const voice = voices.find((v) => v.name === "Google US English");
-speech.voice = voice;
+const voices = window.speechSynthesis.getVoices()
+const voice = voices.find((v) => v.name === "Google US English")
+speech.voice = voice
 
 
 
