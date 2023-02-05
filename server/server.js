16d0db0de06656768b48770a9e34cc5135aab120
Sebastian Lupascu <91190993+Golack24@users.noreply.github.com>
2023-02-05 17:28:39 +0000

    f

--- server/server.js
+++ server/server.js
@@ -42,7 +42,7 @@ app.post('/', async (req, res) => {
 
 
       res.status(200).send({
-        bot: response.data.choices[0].text.replace("JABE:", "")
+        bot: response.data.choices[0].text.replace("AI:", "")
       });
 
     } catch (error) {
